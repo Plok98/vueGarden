3326e0f6e324312685e013aab85db1d6159a6d79
Pleun <pleun.alferink@dealerdirect.com>
2025-12-30 14:23:22 +0100

    Build for deployment

--- src/i18n/en/navigation.ts
+++
@@ -1,9 +0,0 @@
-import type { TranslationSet } from '@/types'
-
-export const navigation: TranslationSet = {
-  switchLanguage: '🇳🇱 Nederlands',
-  discover: 'Discover',
-  myGarden: 'My Garden',
-  pastPlants: 'Plant Overview',
-  profile: 'Dashboard',
-}
--- src/i18n/nl/navigation.ts
+++
@@ -1,9 +0,0 @@
-import type { TranslationSet } from '@/types'
-
-export const navigation: TranslationSet = {
-  switchLanguage: '🇬🇧 English',
-  discover: 'Ontdek',
-  myGarden: 'Mijn Tuin',
-  pastPlants: 'Plantenoverzicht',
-  profile: 'Dashboard',
-}
